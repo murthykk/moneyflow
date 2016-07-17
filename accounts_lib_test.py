8dcbd93d6dafe079162b5a1aff3db18958d3be9d
Kiran Murthy <murthykk@gmail.com>
2016-07-16 20:23:58 -0700

    started accounts test

---
+++ accounts_lib_test.py
@@ -0,0 +1,33 @@
+"""Tests for accounts_lib."""
+
+import mock
+
+import accounts_lib
+from google.apputils import basetest
+import gflags as flags
+import test_utils
+
+
+FLAGS = flags.FLAGS
+
+
+class AccountsListTest(basetest.TestCase):
+
+  def setUp(self):
+    self._fake_storage = test_utils.FakeStorageTable(
+        "accounts", ["account_name", "account_number"])
+    mock.patch.object(
+        accounts_lib.storage_lib, "GetStorageTable",
+        return_value=self._fake_storage).start()
+    self.addCleanup(mock.patch.stopall)
+    self._accounts = accounts_lib.AccountList()
+
+  def testSomething(self):
+    pass
+
+class AccountTest(basetest.TestCase):
+  pass
+
+
+if __name__ == "__main__":
+  basetest.main()
---
+++ test_utils.py
@@ -0,0 +1,25 @@
+"""Utilities for unit testing."""
+
+
+import storage_lib
+
+
+class FakeStorageTable(storage_lib.StorageTable):
+  """Fake storage class used for testing."""
+
+  _fake_rows = None
+
+  def __init__(self, table_name, columns, **kwargs):
+    super(FakeStorageTable, self).__init__(table_name, columns)
+    if "fake_rows" in kwargs.keys():
+      self._fake_rows = kwargs["fake_rows"]
+    else:
+      self._fake_rows = []
+
+  def _ReadRows(self):
+    for row in self._fake_rows:
+      yield row
+
+  def _WriteBuffer(self):
+    self._fake_rows += self._buffer
+    self._buffer = []
