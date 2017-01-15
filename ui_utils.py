741498e283b182c0644d85ac54dc637988eeb118
Kiran Murthy <murthykk@gmail.com>
2017-01-15 00:07:29 -0800

    Finished categorization loop.

--- ui_utils.py
+++ ui_utils.py
@@ -1,6 +1,7 @@
 """Utilities that help interface with users."""
 
 
+import categories_lib
 from collections import deque
 import tabulate
 
@@ -11,6 +12,28 @@ def PromptUser(msg):
     return True
   else:
     return False
+
+
+def GetIntegerFromUser(msg, lower, upper):
+  """Prompt user to enter an integer within bounds.
+
+  Args:
+    msg: Message to display when prompting the user.
+    lower: Lower bound on the integer (inclusive).
+    upper: Upper bound on the integer (inclusive).
+
+  Returns:
+    The integer entered by the user.
+
+  Raises:
+    ValueError: If user entered invalid input.
+  """
+  idx = raw_input("{} [{} - {}]: ".format(msg, lower, upper))
+  idx = int(idx)
+  if idx < lower or idx > upper:
+    raise ValueError(
+        "Out of range: {} is not in [{} - {}]".format(idx, lower, upper))
+  return idx
 
 
 def CategorizeTransactions(transactions):
@@ -26,23 +49,12 @@ def CategorizeTransactions(transactions):
     "No transactions to categorize."
     return
 
-  # Read all categories.
   cat_table = CategoriesTable()
   cat_table.ReadAll(overwrite=True)
-  cat_table.InitializeCategoryLookup()
 
-  # For every transaction, check if a category exists.
-  categories = deque()
-  for txn in transactions:
-    categories.append(cat_table.GetCategoryForTransaction(txn))
-
-  # Print results
-  print "Transactions and their initial categories:"
-  PrintTransactionCategories(zip(transactions, categories))
-
-  # Offer user chance to add categories.
-  if PromptUser("Add categories to transactions without categories?"):
-    AddCategoriesToTransactions(cat_table, transactions)
+  cat_table = AddCategoriesToTransactions(cat_table, transactions)
+  print "Saving any newly added categories."
+  cat_table.Save()
 
 
 def PrintTransactionCategories(transactions_and_categories):
@@ -73,8 +85,8 @@ def PrintTransactionCategories(transactions_and_categories):
         "None"
       ]
 
-  table = ["Date", "Description", "Amount", "Category"] + [
-    get_cols(t) for t in transactions_and_categories
+  table = ["Index", "Date", "Description", "Amount", "Category"] + [
+    [idx + 1] + get_cols(t) for idx,t in enumerate(transactions_and_categories)
   ]
 
   print tabulate.tabulate(table, headers="firstrow", tablefmt="psql")
@@ -96,3 +108,54 @@ def AddCategoriesToTransactions(cat_table, transactions):
   Returns:
     Updated CategoryTable.
   """
+  while True:
+    # For every transaction, check if a category exists.
+    cat_table.InitializeCategoryLookup()
+    categories = deque()
+    for txn in transactions:
+      categories.append(cat_table.GetCategoryForTransaction(txn))
+
+    print "Transactions and their categories:"
+    PrintTransactionCategories(zip(transactions, categories))
+
+    if None not in set(categories):
+      print "All transactions have categories - quitting categorization."
+      break
+
+    # Offer user chance to add categories.
+    if PromptUser("Add a category?"):
+      try:
+        idx = GetIntegerFromUser(
+            "Select a transaction index", 1, len(transactions))
+      except ValueError as e:
+        print "Invalid input. Problem: %s" % str(e)
+        continue
+      cat = GetCategoryFromUser(transactions[idx])
+      print "New Category:"
+      PrettyPrintCategory(cat)
+      if PromptUser("Add this category?"):
+        cat_table.Add(cat)
+    else:
+      print "Quitting transaction categorization."
+      break
+
+  return cat_table
+
+
+def GetCategoryFromUser(transaction_description):
+  """Asks user for category information, and returns a Category object."""
+  print(
+    "What is the category associated with transactions that have the following "
+    "description?")
+  print(transaction_description)
+  category = raw_input("Enter the category's name: ")
+  display_name = raw_input("Enter the display name for the transaction: ")
+  return categories_lib.Category(
+      transaction_description, display_name, category)
+
+
+def PrettyPrintCategory(category):
+  """Pretty prints information in a Category object."""
+  tmp = categories_lib.CategoriesTable()
+  tmp.Add(cat)
+  tmp.Print()
