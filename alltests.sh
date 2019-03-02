2961b21c8cc7d91630d70caca559c6e6b781bd60
Kiran Murthy <murthykk@gmail.com>
2019-03-02 13:16:53 -0800

    Reorganize files to prep for Bazel build.

--- alltests.sh
+++ alltests.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "storage_lib_test.py"
-python storage_lib_test.py
+python moneyflow/storage_lib_test.py
 
 echo "accounts_lib_test.py"
-python accounts_lib_test.py
+python moneyflow/accounts_lib_test.py
 
 echo "transactions_lib_test.py"
-python transactions_lib_test.py
+python moneyflow/transactions_lib_test.py
 
 echo "categories_lib_test.py"
-python categories_lib_test.py
+python moneyflow/categories_lib_test.py
 
 echo "ui_utils_test.py"
-python ui_utils_test.py
+python moneyflow/ui_utils_test.py
