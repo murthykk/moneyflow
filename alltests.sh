80b415e708115c89ed08aa2cee3cb623af4a1e68
Kiran Murthy <murthykk@gmail.com>
2019-03-02 13:35:47 -0800

    Update alltests.sh

--- alltests.sh
+++ alltests.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# Requires python 3.
+# Download from https://www.python.org/downloads/
+
 echo "storage_lib_test.py"
-python moneyflow/storage_lib_test.py
+python3 moneyflow/storage_lib_test.py
 
 echo "accounts_lib_test.py"
-python moneyflow/accounts_lib_test.py
+python3 moneyflow/accounts_lib_test.py
 
 echo "transactions_lib_test.py"
-python moneyflow/transactions_lib_test.py
+python3 moneyflow/transactions_lib_test.py
 
 echo "categories_lib_test.py"
-python moneyflow/categories_lib_test.py
+python3 moneyflow/categories_lib_test.py
 
 echo "ui_utils_test.py"
-python moneyflow/ui_utils_test.py
+python3 moneyflow/ui_utils_test.py
